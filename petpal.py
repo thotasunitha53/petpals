6ac594239ee6b99f9b04cfff519e9910108ae078
Mani <venkatamanikrishnalam@gmail.com>
2024-04-03 17:12:49 -0400

    all fields code

--- petpal.py
+++ petpal.py
@@ -35,13 +35,13 @@ class PetPals:
         self.connection.commit()
 
     def schedule_appointment(self, pet_owner, pet_name, appointment_time, address, appointment_type):
+        # Check if all input fields are provided
+        if not all([pet_owner, pet_name, appointment_time, address, appointment_type]):
+            return "All input fields are mandatory. Please provide values for all fields."
+
         current_time = datetime.now()
         print(current_time)
         print(appointment_time)
-
-        # Parse the first datetime string into a datetime object
-        #current_time = datetime.strptime(datetime.now(), '%Y-%m-%d %H:%M:%S.%f')
-
         # Parse the second datetime string into a datetime object
         appointment_time = datetime.strptime(appointment_time, '%Y-%m-%dT%H:%M')
 
